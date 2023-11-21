4cbb9db33ed0e5acf4a62a03be508f16c632775b
unknown <funmilolaadebiyi52@gmail.com>
2023-11-21 16:32:47 +0100

    nav-bar-updates

--- src/App.vue
+++ src/App.vue
@@ -17,5 +17,27 @@ export default {
 </script>
 
 <style scoped>
-
+ *{
+  margin:0;
+} 
+div{
+  height:100vh;
+  margin:0;
+}
+#app{
+  background-image:url(@/assets/background-home-desktop.jpg);
+  height:100%;
+  width:100%;
+  margin:0;
+}
+@media (min-width:600px)and (max-width:1024px){
+  #app{
+    background-image: url(@/assets/background-home-tablet.jpg);
+  }
+}
+@media (max-width:599px) {
+  #app{
+    background-image: url(@/assets/background-home-mobile.jpg);
+  }
+}
 </style>
